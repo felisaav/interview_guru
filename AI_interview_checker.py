2a6ebd1a0aed4b26aadf34b865f07e5341cf79b3
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-01 11:50:39 -0300

    Add files via upload

---
+++ AI_interview_checker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+if uploaded_file is not None:
+    df = extract_data(uploaded_file)
+
+
+# In[ ]:
+
+
+st.dataframe(df)
+
