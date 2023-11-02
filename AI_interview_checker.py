31d3be7d8727cf3dae984425e27b350d70f60abb
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-02 10:53:17 -0300

    Update AI_interview_checker.py

--- AI_interview_checker.py
+++ AI_interview_checker.py
@@ -1,25 +1,21 @@
 import streamlit as st
 import pandas as pd
-import PyMuPDF #as fitz
+import PyMuPDF
 
 def extract_data(uploaded_file):
     # Initialize a variable to store the extracted text
     pymupdf_text = ""
-
     # Open the PDF file
     pdf_document = PyMuPDF.open(uploaded_file)
-
     # Iterate through each page and extract text
     for page_num in range(pdf_document.page_count):
         page = pdf_document[page_num]
         text = page.get_text()
         pymupdf_text += text
-
     # Close the PDF document
     pdf_document.close()
-
     return pymupdf_text
-    
+
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
     df = extract_data(uploaded_file)
