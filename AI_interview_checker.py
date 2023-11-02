76d201af24bef76059a36e217df9e10049de9d05
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-02 11:30:21 -0300

    Update AI_interview_checker.py

--- AI_interview_checker.py
+++ AI_interview_checker.py
@@ -1,24 +1,24 @@
 import streamlit as st
 import pandas as pd
-import fitz
 
-def extract_data(uploaded_file):
-    # Initialize a variable to store the extracted text
-    pymupdf_text = ""
-    # Open the PDF file
-    pdf_document = fitz.open(uploaded_file)
-    # Iterate through each page and extract text
-    for page_num in range(pdf_document.page_count):
-        page = pdf_document[page_num]
-        text = page.get_text()
-        pymupdf_text += text
-    # Close the PDF document
-    pdf_document.close()
-    return pymupdf_text
+import fitz  # PyMuPDF
 
-uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
-if uploaded_file is not None:
-    df = extract_data(uploaded_file)
-    st.write(df)
+path = "Resume_FelipeSaavedra_2022.pdf"
 
+# Initialize a variable to store the extracted text
+pymupdf_text = ""
 
+# Open the PDF file
+pdf_document = fitz.open(path)
+
+# Iterate through each page and extract text
+for page_num in range(pdf_document.page_count):
+    page = pdf_document[page_num]
+    text = page.get_text()
+    pymupdf_text += text
+
+# Close the PDF document
+pdf_document.close()
+
+# Now you can use the pymupdf_text variable as needed
+print(pymupdf_text)  # Optionally, you can print the extracted text
