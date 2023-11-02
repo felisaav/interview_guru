62d73932c5af030d3b347b95c20c4025d31884cb
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-02 09:55:22 -0300

    Update AI_interview_checker.py

--- AI_interview_checker.py
+++ AI_interview_checker.py
@@ -2,25 +2,6 @@ import streamlit as st
 import pandas as pd
 import fitz
 
-
-# Define a function to extract data from a PDF file
-#def extract_data(uploaded_file):
-    # Create a PDF file reader
-#    pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
-
-    # Initialize an empty list to store extracted data
-#    data = []
-
-    # Iterate through each page in the PDF
-#    for page_num in range(pdf_reader.getNumPages()):
-#        page = pdf_reader.getPage(page_num)
-#        text = page.extract_text()
-#        data.append(text)
-
-    # Create a DataFrame from the extracted data
-#    df = pd.DataFrame(data, columns=["Extracted Text"])
-#    return df
-
 def extract_data(uploaded_file):
     # Initialize a variable to store the extracted text
     pymupdf_text = ""
