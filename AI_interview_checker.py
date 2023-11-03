ab015058275190b84de562e41c623dd0cfa0598e
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-03 00:41:54 -0300

    Update AI_interview_checker.py

--- AI_interview_checker.py
+++ AI_interview_checker.py
@@ -2,14 +2,29 @@ import streamlit as st
 import pandas as pd
 import openai
 import os
-
+import fitz  # PyMuPDF
 
 with st.form(key ='Form1'):
     uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+    #----------
+    # Initialize a variable to store the extracted text
+    pymupdf_text = ""
+    # Open the PDF file
+    pdf_document = fitz.open(uploaded_file)
+    
+    # Iterate through each page and extract text
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document[page_num]
+        text = page.get_text()
+        pymupdf_text += text
+    # Close the PDF document
+    pdf_document.close()
+    #----------
     #if uploaded_file is not None:
     #    df = extract_data(uploaded_file)
     #    st.write(df)
     #load position info
+    
     position_title=st.text_input('job position')
     description=st.text_input('job description')
     
