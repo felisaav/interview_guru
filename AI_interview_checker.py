236b9353e4fe53a116563e61db1891aede0da231
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-03 00:53:08 -0300

    Update AI_interview_checker.py

--- AI_interview_checker.py
+++ AI_interview_checker.py
@@ -5,20 +5,15 @@ import os
 import fitz  # PyMuPDF
 
 with st.form(key ='Form1'):
-    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
-    #----------
-    # Initialize a variable to store the extracted text
-    pymupdf_text = ""
-    # Open the PDF file
-    pdf_document = fitz.open(uploaded_file)
-    
-    # Iterate through each page and extract text
-    for page_num in range(pdf_document.page_count):
-        page = pdf_document[page_num]
-        text = page.get_text()
-        pymupdf_text += text
-    # Close the PDF document
-    pdf_document.close()
+    uploaded_pdf = st.file_uploader("Load pdf: ", type=['pdf'])
+
+    if uploaded_pdf is not None:
+        doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
+        text = ""
+        for page in doc:
+            text += page.getText()
+        st.write(text) 
+        doc.close()
     #----------
     #if uploaded_file is not None:
     #    df = extract_data(uploaded_file)
