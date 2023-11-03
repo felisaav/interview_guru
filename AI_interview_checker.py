aaa88ea6a7e6ed17c5475508ab609ea9951cef3b
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-03 00:36:02 -0300

    Update AI_interview_checker.py

--- AI_interview_checker.py
+++ AI_interview_checker.py
@@ -42,8 +42,7 @@ if submit_code:
         messages=[{"role": "system",
                    "content": role},
                   {"role": "user",
-                   "content": instr_1 + "curriculum:"+uploaded_file + "Cargo a postular:"+
-                              position_title + "Descripción cargo:"+description}]
+                   "content": instr_1 + "curriculum:"+uploaded_file + "Cargo a postular:"+ position_title + "Descripción cargo:"+description}]
     )
 
     st.text(response["choices"][0]["message"]["content"])
