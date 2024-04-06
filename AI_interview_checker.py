df1a32fbbaf4bcbe3371bb6cf57a0bca18a68109
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2024-04-05 22:36:07 -0300

    Update AI_interview_checker.py

--- AI_interview_checker.py
+++ AI_interview_checker.py
@@ -123,22 +123,27 @@ with tab0: #NLP group project
             j_d=embedding_vector(job_description)
 
             #Analyze resumes
-            resumes=pd.DataFrame()
+
             for i in range(len(text)):
-                if 'resumes' not in globals():
-                    resumes = pd.DataFrame(columns=["name", "email", "skills", "education", "experience", "years_of_experience"])
-                # Append the new resume data to the DataFrame
-                resumes = resumes.append(preprocess_cv(text[i]), ignore_index=True)
+                st.write(preprocess_cv(text[i]))
 
-            resumes['merged_info'] = resumes.apply(merge_columns, axis=1)
+            
+            #resumes=pd.DataFrame()
+            #for i in range(len(text)):
+            #    if 'resumes' not in globals():
+            #        resumes = pd.DataFrame(columns=["name", "email", "skills", "education", "experience", "years_of_experience"])
+            #    # Append the new resume data to the DataFrame
+            #    resumes = resumes.append(preprocess_cv(text[i]), ignore_index=True)
 
-            # Apply the embedding_vector function to the "merged_info" column
-            resumes['embedding'] = resumes['merged_info'].apply(embedding_vector)
+            #resumes['merged_info'] = resumes.apply(merge_columns, axis=1)
+
+            ## Apply the embedding_vector function to the "merged_info" column
+            #resumes['embedding'] = resumes['merged_info'].apply(embedding_vector)
             
-            # Apply the similarity_score function between the resulting embeddings and the constant vector 'j_d'
-            resumes['score'] = resumes.apply(lambda row: similarity_score(row['embedding'], j_d), axis=1)
+            ## Apply the similarity_score function between the resulting embeddings and the constant vector 'j_d'
+            #resumes['score'] = resumes.apply(lambda row: similarity_score(row['embedding'], j_d), axis=1)
             
-            st.write(resumes.sort_values(by='score', ascending=False))
+            #st.write(resumes.sort_values(by='score', ascending=False))
 
 
 
